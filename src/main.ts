fada7d6e60c365944353e8a367b3ab358177a18f
Kaym Kassai <kassai.kaym@gmail.com>
2022-10-28 12:22:55 +0200

    Fix cors issue

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   app.use(json({ limit: '100mb' }));
   app.use(helmet());
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
     credentials: true,
   });
 
